ffe6c02f165462f089a6f736f95ebdad08db0d21
Bokseongmin <bokseongmin@gmail.com>
2023-04-27 15:11:04 +0900

    1st

---
+++ src/main/java/com/uracle/sample/IitpSwApplication.java
@@ -0,0 +1,13 @@
+package com.uracle.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IitpSwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IitpSwApplication.class, args);
+    }
+
+}
